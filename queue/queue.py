841d2cb7a4b9a45fe06aa9cce86c6cec28668c91
Talent Antonio <antonio.talenty@gmail.com>
2019-06-10 16:19:06 +0100

    Imports and Paths

--- queue/queue.py
+++ queue/queue.py
@@ -1,4 +1,6 @@
-from linked_list import Linked_list
+import sys
+sys.path.append('../linked_list')
+from linked_list import LinkedList
 
 class Queue:
   def __init__(self):
@@ -8,18 +10,18 @@ class Queue:
     # insert 
     # new array if its full
     # create then import linked list 
-    self.storage = Linked_list
+    self.storage = linked_list.LinkedList
 
   def enqueue(self, item):
     pass
     # add item
-    return self.storage.insert(0, item)
+    self.storage.insert(0, item)
   
   def dequeue(self):
     pass
     # remove item
     return self.storage.pop()
-
+ 
   def len(self):
     pass
     # counter
