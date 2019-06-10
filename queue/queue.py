f643e10147ddf38128dd3b18547f72d8665d3618
Talent Antonio <antonio.talenty@gmail.com>
2019-06-10 15:51:18 +0100

    added Linked list

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,5 @@
+from linked_list import Linked_list
+
 class Queue:
   def __init__(self):
     self.size = 0
@@ -5,8 +7,8 @@ class Queue:
     # use to store queue elements?
     # insert 
     # new array if its full
-    # linked list
-    self.storage = []
+    # create then import linked list 
+    self.storage = Linked_list
 
   def enqueue(self, item):
     pass
