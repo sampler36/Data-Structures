0e4bd7859bbc9151c7a820ee741f96887a03c865
Talent Antonio <antonio.talenty@gmail.com>
2019-06-12 23:06:19 +0100

    buble up / max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -15,7 +15,22 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+     # in worst case element will need to make way to top of heap
+     while index > 0:
+       # get parent element
+       parent = (index - 1) // 2
+            # check if elem at index is highter priority than parent
+       if self.storage[index] > self.storage[parent]:
+         # if it is then swap
+        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+
+        # update index to be new spot that swapped elem now resides at
+        index = parent
+
+       else:
+        # otherwise our elem is at valid spot
+        # we no longer need to bubble up
+           break
 
   def _sift_down(self, index):
     pass
